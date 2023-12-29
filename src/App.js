0218dbf29422cb2bb3dbafda76b5db640e86ce2b
Jan Stastny <honza@stastnarodina.com>
2023-12-29 09:38:58 +0100

    Add logo to home screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
+import logo from "./baby.svg";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState("");
@@ -20,7 +21,7 @@ function App() {
   const calculateWeekAndDay = () => {
     if (selectedDate) {
       const today = moment();
-      console.log({"Selected date: ": moment(selectedDate)})
+      console.log({ "Selected date: ": moment(selectedDate) });
       const conceptionDate = moment(selectedDate).subtract(40, "weeks");
       const pregnancyDurationDays = today.diff(conceptionDate, "days");
       const weeks = Math.floor(pregnancyDurationDays / 7);
@@ -33,6 +34,9 @@ function App() {
 
   return (
     <div className="bg-custom-gray min-h-screen flex flex-col justify-center items-center font-sans">
+      <div class="w-1/5">
+        <img src={logo} alt="Logo"></img>
+      </div>
       <h1 className="text-3xl mb-8 text-custom-blue">Kalkulačka početí</h1>
       <input
         type="date"
@@ -41,12 +45,13 @@ function App() {
         onChange={handleDateChange}
         placeholder="Předpokládaný termín porodu"
       />
-        <h2 className="text-2xl mb-2 text-custom-blue">Datum početí</h2>
-        <p className="text-2xl mb-2 text-custom-blue">{calculateConceptionDate()}</p>
-      
-        <h2 className="text-2xl mb-2 text-custom-blue">Týden těhotenství</h2>
-        <p className="text-2xl mb-2 text-custom-blue">{calculateWeekAndDay()}</p>
-      
+      <h2 className="text-2xl mb-2 text-custom-blue">Datum početí</h2>
+      <p className="text-2xl mb-2 text-custom-blue">
+        {calculateConceptionDate()}
+      </p>
+
+      <h2 className="text-2xl mb-2 text-custom-blue">Týden těhotenství</h2>
+      <p className="text-2xl mb-2 text-custom-blue">{calculateWeekAndDay()}</p>
     </div>
   );
 }
